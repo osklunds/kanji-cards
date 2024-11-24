0ca484170933164ecca01d020d240c28292e6faf
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-11-24 17:06:20 +0100

    Add some xml require statements to test case

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,12 +1,26 @@
 
 #include <iostream>
+#include <string.h>
 #include "catch.hpp"
+#include "pugixml.hpp"
 
 #ifdef TEST
 
-TEST_CASE("hej") {
-    std::cout << "hejjjj" << std::endl;
-    REQUIRE(1+1 == 2);
+TEST_CASE("learning") {
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file("sample.xml");
+    REQUIRE(result == true);
+
+    pugi::xml_node note = doc.child("note");
+    const char *name = note.name();
+    REQUIRE(strcmp(name, "note") == 0);
+
+    pugi::xml_node child = note.child("some_tag");
+    REQUIRE(strcmp(child.name(), "some_tag") == 0);
+
+    pugi::xml_text text = child.text();
+    const char *text_as_char = text.get();
+    REQUIRE(strcmp(text_as_char, "Hello") == 0);
 }
 
 #endif
