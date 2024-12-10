c9bacb8b901bb84bdd092d830f3cc4955224a671
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-12-10 19:38:19 +0100

    Use sample frequency file in test

--- src/frequency_tests.cpp
+++ src/frequency_tests.cpp
@@ -9,13 +9,13 @@
 
 TEST_CASE("get_frequency_entries_from_file") {
     std::vector<frequency_entry> entries =
-        get_frequency_entries_from_file("../data/kanji_freqency_list.csv");
+        get_frequency_entries_from_file("../data/kanji_freqency_list_sample.csv");
 
-    REQUIRE(entries[0].get_frequency() == 11);
+    REQUIRE(entries[0].get_frequency() == 10);
     REQUIRE(entries[0].get_kanji() == "一");
-    REQUIRE(entries[1].get_frequency() == 128);
+    REQUIRE(entries[1].get_frequency() == 432);
     REQUIRE(entries[1].get_kanji() == "二");
-    REQUIRE(entries[2].get_frequency() == 1254);
+    REQUIRE(entries[2].get_frequency() == 97);
     REQUIRE(entries[2].get_kanji() == "仁");
 }
 
