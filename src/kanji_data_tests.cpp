1ac35d1feb064b9eefe453d426994cc5cfb31c74
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-12-31 13:20:56 +0100

    Remove usage of kanjidic2_sample

--- src/kanji_data_tests.cpp
+++ src/kanji_data_tests.cpp
@@ -10,7 +10,7 @@
 TEST_CASE("sample_data") {
     pugi::xml_document kanjidic2_doc;
     pugi::xml_parse_result result_kanjidic2 =
-        kanjidic2_doc.load_file("../data/kanjidic2_sample.xml");
+        kanjidic2_doc.load_file("../data/kanjidic2.xml");
     REQUIRE(result_kanjidic2 == true);
 
     pugi::xml_document jmdict_e_doc;
@@ -66,7 +66,7 @@ TEST_CASE("sample_data") {
 TEST_CASE("as_string") {
     pugi::xml_document kanjidic2_doc;
     pugi::xml_parse_result result_kanjidic2 =
-        kanjidic2_doc.load_file("../data/kanjidic2_sample.xml");
+        kanjidic2_doc.load_file("../data/kanjidic2.xml");
     REQUIRE(result_kanjidic2 == true);
 
     pugi::xml_document jmdict_e_doc;
