fb01fed3656b058be117aa3240ac9ffb25e5dbb2
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-12-31 11:24:55 +0100

    Add todo about multithread

--- src/create_card_tests.cpp
+++ src/create_card_tests.cpp
@@ -65,6 +65,8 @@ TEST_CASE("run_main", "[.]") {
     REQUIRE(!std::filesystem::exists(out_dir));
     REQUIRE(std::filesystem::create_directory(out_dir));
 
+    // Since this loop is by far the bottleneck, consider using multiple
+    // threads.
     for (int i = 0; i < entries.size(); i++) {
         auto fe = entries[i];
         kanji_data kanji_data { fe.get_kanji(),
