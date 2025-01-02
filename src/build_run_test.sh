a639e7d3ed8185201def4aeaa1132a40867987f4
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-01-02 17:31:00 +0100

    Create build dir if it doesn't exist

--- src/build_run_test.sh
+++ src/build_run_test.sh
@@ -6,6 +6,8 @@
 # and ./build_run_test.sh run_main to run the main function (which happens to
 # be a test case too)
 
-cd "$(git rev-parse --show-toplevel)/build"
+build_dir="$(git rev-parse --show-toplevel)/build"
+mkdir -p "$build_dir"
+cd "$build_dir"
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. && make -j8 && ./main "$@"
