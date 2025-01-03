88c611d6a496dd5d632a7c8e21dc163787f0384e
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-01-03 13:02:25 +0100

    Add copyright notice and license notice to all src files

--- src/create_JMdict_e.xml.sample.sh
+++ src/create_JMdict_e.xml.sample.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Copyright (C) 2025 Oskar Lundström
+
+# This file is part of kanji-cards.
+
+# kanji-cards is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# kanji-cards is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with
+# kanji-cards. If not, see <https://www.gnu.org/licenses/>.
+
+
 # Script for creating JMdict_e.xml.sample, which contains only a subset of
 # the words from the full JMdict_e.xml.sample. If the full data is used too
 # many words are found (per kanji) which makes the tests unwieldy.
--- src/util.hpp
+++ src/util.hpp
@@ -1,4 +1,21 @@
 
+// Copyright (C) 2025 Oskar Lundström
+
+// This file is part of kanji-cards.
+
+// kanji-cards is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+
+// kanji-cards is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+
+// You should have received a copy of the GNU General Public License along with
+// kanji-cards. If not, see <https://www.gnu.org/licenses/>.
+
 /*
   Utility functions not really domain-specific or related to a single other
   file.
